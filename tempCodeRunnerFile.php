175cc3812b43c1f7f535b9d6677ffa5fcddf86ce
Secret <toontown@inbox.ru>
2019-04-14 12:54:28 +0300

    <?php

---
+++ tempCodeRunnerFile.php
@@ -0,0 +1,5 @@
+<?php
+<p>#user_name#</p>
+            <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+            <a class="user-menu__logout" href="#">Выход</a>
+        </div>
