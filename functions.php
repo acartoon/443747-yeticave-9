4f9fef2551c5eafa7f92f02d7dd4be3aa2c2ef0f
Secret <toontown@inbox.ru>
2019-04-22 20:48:15 +0300

    функция времени

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+
+function formatPrice($num) {
+    $result = number_format((ceil($num)), 0, '.', ' ');
+    $result .= '<b class="rub">р</b>';
+    return $result;
+};
+
+function timer($date) {
+    $dt_end = date_create($date);
+    $dt_now = date_create("now");
+    $dt_diff = date_diff($dt_now, $dt_end);
+    $timer_days = date_interval_format($dt_diff, "%a");
+    $result = '';
+   
+    if($dt_now > $dt_end) {
+        $result = 'лот закрыт';
+    } elseif($timer_days < 1 and $dt_now < $dt_end) {
+        $result =  date_interval_format($dt_diff, "%h ч %i мин");
+    } elseif(0 < $timer_days and $timer_days < 6) {
+        $result =  date_interval_format($dt_diff, "%d дн");
+    } elseif(5 < $timer_days) {
+        $result =  date_interval_format($dt_diff, "%d дн %m м %y лет");
+    }
+    return $result;
+};
+?>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'helpers.php';
+require_once 'functions.php';
 
 $is_auth = rand(0, 1);
 
@@ -8,64 +9,64 @@ $user_name = 'Секрет'; // укажите здесь ваше имя
 
 
 $category = [
-    0 => [
+    [
         'class' => 'boards',
         'name' => 'Доски и лыжи'
     ],
-    1 => [
+    [
         'class' => 'attachment',
         'name' => 'Крепления'
     ],
-    2 => [
+    [
         'class' => 'boots',
         'name' => 'Ботинки'
     ],
-    3 => [
+    [
         'class' => 'clothing',
         'name' => 'Одежда'
     ],
-    4 => [
+    [
         'class' => 'tools',
         'name' => 'Инструменты'
     ],
-    5 => [
+    [
         'class' => 'other',
         'name' => 'Разное'
     ]
 ];
 
 $lots = [
-    $lot1 = [
+    [
         'name' => '2014 Rossignol District Snowboard',
         'category' => 'Доски и лыжи',
         'price' => '10999',
         'URL' => 'img/lot-1.jpg'
     ],
-    $lot2 = [
+    [
         'name' => 'DC Ply Mens 2016/2017 Snowboard',
         'category' => 'Доски и лыжи',
         'price' => '159999',
         'URL' => 'img/lot-2.jpg'
-],
-    $lot3 = [
+    ],
+    [
         'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
         'category' => 'Крепления',
         'price' => '8000',
         'URL' => 'img/lot-3.jpg'
-],
-    $lot4 = [
+    ],
+    [
         'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
         'category' => 'Ботинки',
         'price' => '10999',
         'URL' => 'img/lot-4.jpg'
-],
-    $lot5 = [
+    ],
+    [
         'name' => 'Куртка для сноуборда DC Mutiny Charocal',
         'category' => 'Одежда',
         'price' => '7500',
         'URL' => 'img/lot-5.jpg'
-],
-    $lot6 = [
+    ],
+    [
         'name' => 'Маска Oakley Canopy',
         'category' => 'Разное',
         'price' => '5400',
@@ -73,25 +74,7 @@ $lots = [
     ]
 ];
 
-
-//module2-task2
-
-$formatNumber = function($num) {
-    $result = number_format((ceil($num)), 0, '.', ' ');
-    $result .= '<b class="rub">р</b>';
-    return $result;
-};
-
-// function formatNumber($num) {
-//     $noFloat = ceil($num);
-//     //print 'строка'.$noFloat;
-//     $part1 = substr($nofloat, 0, -3);
-//     $part2 = substr($nofloat, -3, 3);
-//    // $result = $part1 .' '. $part2 . ' ' .'₽';
-//     $result = $part2;
-//     return $result;
-// };
-$main_content = include_template('index.php', ['category' => $category, 'lots' => $lots, 'formatNumber' => $formatNumber]);
+$main_content = include_template('index.php', ['category' => $category, 'lots' => $lots]);
 
 $index_page = include_template('layout.php', 
 ['category' => $category, 'main_content' => $main_content, 'title' => 'Главная', 'is_auth' => $is_auth, 'user_name' => $user_name]);
