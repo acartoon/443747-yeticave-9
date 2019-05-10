8bc4aa984a79217a9e0f98a28b83b765e4b38079
Secret <toontown@inbox.ru>
2019-05-10 11:58:32 +0300

    исправление ошибок

--- sign-up.php
+++ sign-up.php
@@ -14,35 +14,51 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     }
 
+    if(isset($user['password'])) {
+        if(strlen($user['password']) > 12) {
+            $errors['password'] = 'Слишком длинный пароль. Максимальная длина 12 символов';
+        }
+    }
+
+    if(isset($user['name'])) {
+        if(strlen($user['name']) > 64) {
+            $errors['name'] = 'Слишком длинное имя';
+        }
+    }
+    
+    if(isset($user['message'])) {
+        if(strlen($user['message']) > 200) {
+            $errors['message'] = 'Максимальная длина символов 200';
+        }
+    }
+
     if(isset($user['email'])) {
         if(!filter_var($user['email'], FILTER_VALIDATE_EMAIL)) {
             $errors['email'] = 'Введите корректный email';
-        } elseif(strlen($user['email']) > 192) {
-            $errors['email'] = 'Слишком длинный email, придумайте короче';
+        } else {
+            $check_pass = 'SELECT * FROM users WHERE email = ?';
+            $stmt =  db_get_prepare_stmt($link, $check_pass, [$user['email']]);
+            mysqli_stmt_execute($stmt);
+            $res = mysqli_stmt_get_result($stmt);
+            $total = $res ? mysqli_fetch_assoc($res) : null;
+            if($total) {
+                $errors['email'] = 'Такой пользователь уже существует!';
+            } 
         }
     }
     
     if(!count($errors)) {
-        $check_pass = 'SELECT * FROM users WHERE email = ?';
-        $stmt =  db_get_prepare_stmt($link, $check_pass, [$user['email']]);
-        mysqli_stmt_execute($stmt);
-        $res = mysqli_stmt_get_result($stmt);
-        $total = $res ? mysqli_fetch_assoc($res): null;
-        if($total) {
-            $errors['email'] = 'Такой пользователь уже существует!';
-        } else {
-            $pass = password_hash($user['password'], PASSWORD_DEFAULT);
-            $sql = 'INSERT INTO users (name, day_registration, email, pass, contacts)
-            VAlUES (?, NOW(), ?, ?, ?)';
-            $stmt = db_get_prepare_stmt($link, $sql, [$user['name'], $user['email'], $pass, $user['message']]);
-            $res = mysqli_stmt_execute($stmt);
-            $res ? header("location: /"): error_404();
-        }
+        $pass = password_hash($user['password'], PASSWORD_DEFAULT);
+        $sql = 'INSERT INTO users (name, email, pass, contacts)
+        VAlUES (?, ?, ?, ?)';
+        $stmt = db_get_prepare_stmt($link, $sql, [$user['name'], $user['email'], $pass, $user['message']]);
+        $res = mysqli_stmt_execute($stmt);
+        $res ? header("location: /"): error_404();
     }
 }
 $main_content = include_template('sign-up.php', ['categories' => $categories, 'errors' => $errors, 'user' => $user]);
 $index_page = include_template('layout.php', 
-['categories' => $categories, 'main_content' => $main_content, 'title' => 'Карточка товара', 'is_auth' => $is_auth, 'user_name' => $user_name]);
+    ['categories' => $categories, 'main_content' => $main_content, 'title' => 'Карточка товара', 'is_auth' => $is_auth, 'user_name' => $user_name]);
 
 print $index_page;
 ?>
